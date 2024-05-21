6316dc08be51a2934ca3ed8082b998cf7d7def11
ticket <mzkhashyar@gmail.com>
2024-05-21 18:11:24 +0330

    db

--- src/mft/model/da/db.sql
+++ src/mft/model/da/db.sql
@@ -1,12 +1,29 @@
-create table customer(
-    id number primary key ,
-    name nvarchar2(30),
-    family nvarchar2(30),
-    gender varchar2(6),
+create table customer
+(
+    id         number primary key,
+    name       nvarchar2(30),
+    family     nvarchar2(30),
+    gender     varchar2(6),
     birth_date date,
-    city varchar2(20)
+    city       varchar2(20)
 );
 
-create sequence person_seq  start with 1 increment by 1;
+create sequence customer_seq start with 1 increment by 1;
 
-create tabl
+create table Event
+(
+    id       number primary key,
+    name     nvarchar2(20),
+    category nvarchar2(20),
+    price    number,
+    quantity number,
+    datetime date
+);
+create sequence Event_seq start with 1 increment by 1;
+create table Payment
+(
+    id       number primary key,
+    amount   number,
+    datetime date
+);
+create sequence Payment_seq start with 1 increment by 1;
