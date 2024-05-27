007edff5be26f35a5769c7bd46ab14330386fe74
khashayar mehdizadeh <mzkhashayar@gmail.com>
2024-05-26 19:02:39 -0700

    update db.sql

--- src/mft/model/da/db.sql
+++ src/mft/model/da/db.sql
@@ -1,7 +1,9 @@
 create table customer
 (
     id          number primary key,
+    name        nvarchar2(30),
     family      nvarchar2(30),
+    email       nvarchar2(30),
     phoneNumber varchar2(11)
 );
 
@@ -9,18 +11,20 @@ create sequence customer_seq start with 1 increment by 1;
 
 create table Event
 (
-    id       number primary key,
-    name     nvarchar2(20),
-    category nvarchar2(20),
-    price    number,
-    quantity number,
-    date_time timestamp
+    id          number primary key,
+    name        nvarchar2(20),
+    category    nvarchar2(20),
+    price       number,
+    capacity    number,
+    description nvarchar2(50),
+    date_time   timestamp
 );
 create sequence Event_seq start with 1 increment by 1;
 create table Payment
 (
-    id       number primary key,
-    amount   number,
+    id        number primary key,
+    amount    number,
+    payment_type references payment,
     date_time timestamp
 );
 create sequence Payment_seq start with 1 increment by 1;
@@ -28,10 +32,10 @@ create sequence Payment_seq start with 1 increment by 1;
 
 create table Ticket
 (
-    id       number primary key,
+    id          number primary key,
     event_id references Event,
-    info nvarchar2(30),
-    date_time timestamp,
+    info        nvarchar2(30),
     payment_id references Payment,
-    customer_id refernces customer
+    customer_id refrences customer,
+    ate_time    timestamp
 );
