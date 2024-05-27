1773ceedb71b1097a3c928c83c5bc4afeb0f965f
khashayar mehdizadeh <mzkhashayar@gmail.com>
2024-05-27 12:35:23 -0700

    update PaymendDa

--- src/mft/model/da/PaymentDa.java
+++ src/mft/model/da/PaymentDa.java
@@ -1,14 +1,13 @@
 package mft.model.da;
 
 import mft.model.entity.Payment;
+import mft.model.entity.enums.PaymentType;
 import mft.model.tools.CRUD;
 import mft.model.tools.ConnectionProvider;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PaymentDa implements AutoCloseable, CRUD<Payment> {
@@ -42,16 +41,34 @@ public class PaymentDa implements AutoCloseable, CRUD<Payment> {
         preparedStatement.setDouble(2,payment.getAmount());
         preparedStatement.setString(3, String.valueOf(payment.getPaymentType()));
         preparedStatement.setTimestamp(4,Timestamp.valueOf(payment.getDateTime()));
-        return null;
+        preparedStatement.execute();
+        return payment;
     }
 
     @Override
     public Payment remove(int id) throws Exception {
+        preparedStatement=connection.prepareStatement(
+                "delete from payment where id=?"
+        );
+        preparedStatement.setInt(1,id);
+        preparedStatement.execute();
         return null;
     }
 
     @Override
     public List<Payment> findAll() throws Exception {
+        List<Payment> paymentList=new ArrayList<>();
+        preparedStatement=connection.prepareStatement("select *from payment order by id");
+        ResultSet resultSet=preparedStatement.executeQuery();
+        while (resultSet.next()){
+            Payment payment=Payment
+                    .builder()
+                    .id(resultSet.getInt("id"))
+                    .amount(resultSet.getDouble("amount"))
+                    .paymentType(PaymentType.valueOf(resultSet.getString("payment_type")))
+                    .dateTime(resultSet.getDate("datetime").toLocalDate().atStartOfDay())
+                    .build();
+        }
         return null;
     }
 
