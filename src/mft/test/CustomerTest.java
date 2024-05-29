624d0bc3a17cd14456051fac00afa27590f3d189
khashayar mehdizadeh <mzkhashayar@gmail.com>
2024-05-29 11:37:28 -0700

    PaymentBl

--- src/mft/test/CustomerTest.java
+++ src/mft/test/CustomerTest.java
@@ -1,16 +1,35 @@
 package mft.test;
 
+import mft.model.bl.CustomerBl;
+import mft.model.bl.EventBl;
+import mft.model.da.CustomerDa;
 import mft.model.entity.Customer;
+import mft.model.entity.Event;
+import mft.model.entity.enums.EventCategory;
+
+import java.time.LocalDateTime;
 
 public class CustomerTest {
     public static void main(String[] args) {
-        Customer.builder()
-                .id(1)
-                .name("khashayar")
-                .family("mehdizadeh")
-                .email("mzkhashayar@gmail.com")
-                .phoneNumber("09120185685")
-                .build();
+        Customer customer=
+                Customer.builder()
+                        .id(1)
+                        .name("khashayar")
+                        .family("mehdizadeh")
+                        .phoneNumber("09120185685")
+                        .email("mzkhashayar@gmail")
+                        .build();
+
+        CustomerDa.get().save(customer);
+        System.out.println(customer);
+
+
+
+        System.out.println(customer);
+
+
+
+
 
     }
 }
