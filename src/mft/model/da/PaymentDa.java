46da4e3702ea89d82629c1a0b26d28f73adf1e0b
khashayar mehdizadeh <mzkhashayar@gmail.com>
2024-06-11 00:14:05 -0700

    update

--- src/mft/model/da/PaymentDa.java
+++ src/mft/model/da/PaymentDa.java
@@ -2,102 +2,75 @@ package mft.model.da;
 
 import lombok.extern.log4j.Log4j;
 import mft.model.entity.Payment;
-import mft.model.entity.enums.PaymentType;
 import mft.model.tools.CRUD;
 import mft.model.tools.ConnectionProvider;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 @Log4j
 public class PaymentDa implements AutoCloseable, CRUD<Payment> {
     private final Connection connection;
     private PreparedStatement preparedStatement;
-
-    public  PaymentDa() throws SQLException {
-        connection = ConnectionProvider.getConnectionProvider().getConnection();
+    public PaymentDa() throws SQLException{
+        connection= ConnectionProvider.getConnectionProvider().getConnection();
     }
 
     @Override
     public Payment save(Payment payment) throws Exception {
-        payment.setId(ConnectionProvider.getConnectionProvider().getNextId("payment_SEQ"));
+        payment.setId(ConnectionProvider.getConnectionProvider().getNextId("PAYMENT_SEQ"));
         payment.setDate_time(LocalDateTime.now());
-        preparedStatement = connection.prepareStatement(
-                "INSERT INTO PAYMENT(ID,AMOUNT,PAYMENT_TYPE,DATE_TIME) VALUES (?,?,?,?)"
-
+        preparedStatement=connection.prepareStatement(
+                "INSERT INTO PAYMENT(ID, AMOUNT, PAYMENT_TYPE, DATE_TIME) VALUES (?,?,?,?)"
         );
-        preparedStatement.setInt(1, payment.getId());
-        preparedStatement.setDouble(2, payment.getAmount());
-        preparedStatement.setString(3, payment.getPayment_type().name());
-        preparedStatement.setTimestamp(4,Timestamp.valueOf(payment.getDate_time()));
+        preparedStatement.setInt(1,payment.getId());
+        preparedStatement.setDouble(2,payment.getAmount());
+        preparedStatement.setString(3,payment.getPayment_type().name());
+        preparedStatement.setTimestamp(4, Timestamp.valueOf(payment.getDate_time()));
         preparedStatement.execute();
         return payment;
     }
 
     @Override
     public Payment edit(Payment payment) throws Exception {
-        preparedStatement = connection.prepareStatement(
-                "UPDATE PAYMENT SET AMOUNT=?,PAYMENT_TYPE,DATE_TIME=? WHERE ID=?"
+        preparedStatement=connection.prepareStatement(
+                "UPDATE PAYMENT SET AMOUNT=?,PAYMENT_TYPE=?,DATE_TIME=? WHERE ID=?"
         );
-        preparedStatement.setInt(1,payment.getId());
-        preparedStatement.setDouble(2, payment.getAmount());
-        preparedStatement.setTimestamp(3, Timestamp.valueOf(payment.getDate_time()));
-        preparedStatement.setString(4, String.valueOf(payment.getPayment_type()));
+        preparedStatement.setDouble(1,payment.getAmount());
+        preparedStatement.setString(2,payment.getPayment_type().name());
+        preparedStatement.setTimestamp(3,Timestamp.valueOf(payment.getDate_time()));
+        preparedStatement.setInt(4,payment.getId());
         preparedStatement.execute();
         return payment;
     }
 
     @Override
     public Payment remove(int id) throws Exception {
-        preparedStatement = connection.prepareStatement(
+        preparedStatement=connection.prepareStatement(
                 "DELETE FROM PAYMENT WHERE ID=?"
         );
-        preparedStatement.setInt(1, id);
+        preparedStatement.setInt(1,id);
         preparedStatement.execute();
         return null;
     }
 
     @Override
     public List<Payment> findAll() throws Exception {
-        List<Payment> paymentList = new ArrayList<>();
-        preparedStatement = connection.prepareStatement("select *from payment order by id");
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            Payment payment = Payment
-                    .builder()
-                    .id(resultSet.getInt("id"))
-                    .amount(resultSet.getDouble("amount"))
-                    .payment_type(PaymentType.valueOf(resultSet.getString("payment_type")))
-                    .date_time(resultSet.getDate("date_time").toLocalDate().atStartOfDay())
-                    .build();
-            paymentList.add(payment);
-        }
-        return paymentList;
+        return null;
     }
 
     @Override
     public Payment findById(int id) throws Exception {
-        preparedStatement = connection.prepareStatement("select *from payment where id=?");
-        preparedStatement.setInt(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        Payment payment = null;
-        if (resultSet.next()) {
-            payment = Payment
-                    .builder()
-                    .id(resultSet.getInt("id"))
-                    .amount(resultSet.getDouble("amount"))
-                    .payment_type(PaymentType.valueOf(resultSet.getString("payment_type")))
-                    .date_time(resultSet.getDate("datetime").toLocalDate().atStartOfDay())
-                    .build();
-
-        }
-        return payment;
+        return null;
     }
-
 
     @Override
     public void close() throws Exception {
+        preparedStatement.close();
+        connection.close();
 
     }
 }
--- src/mft/test/PaymentTest.java
+++ src/mft/test/PaymentTest.java
@@ -1,21 +1,18 @@
 package mft.test;
 
 import mft.model.bl.PaymentBl;
-import mft.model.entity.Payment;
-import mft.model.entity.enums.PaymentType;
-
-import java.time.LocalDateTime;
 
 public class PaymentTest {
     public static void main(String[] args) throws Exception {
-        Payment payment=
-                Payment.builder()
-                       // .id(1)
-                        .amount(275.000)
-                        .payment_type(PaymentType.Card)
-                        .date_time(LocalDateTime.now())
-                        .build();
-        System.out.println(PaymentBl.getPaymentBl().save(payment));
+//        Payment payment=
+//                Payment.builder()
+//
+//                        .amount(150.0)
+//                        .payment_type(PaymentType.Cash)
+//                       // .date_time(LocalDateTime.now())
+//
+//                        .build();
+        System.out.println(PaymentBl.getPaymentBl().remove(1));
         System.out.println("saved");
 
     }
