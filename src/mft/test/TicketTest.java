7bd005b15a239c5356f62ee547e5e1475aa18cd9
khashayar mehdizadeh <mzkhashayar@gmail.com>
2024-06-11 20:03:59 -0700

    test ok!20:03

--- src/mft/test/TicketTest.java
+++ src/mft/test/TicketTest.java
@@ -1,31 +1,62 @@
-//package mft.test;
-//
-//import mft.model.bl.TicketBl;
-//import mft.model.entity.Ticket;
-//
-//import java.time.LocalDateTime;
-//
-//public class TicketTest {
-//    public static void main(String[] args) throws Exception {
-//
-//        Ticket ticket=
-//                Ticket
-//                        .builder()
-//                        .info("jurassic")
-//                        .date_Time(LocalDateTime.now())
-//                        .info("just entry")
-//
-//
-//
-//
-//
-//
-////
-//                        .build();
-//
-//        System.out.println(    TicketBl.getTicketBl().save(ticket));
-//        System.out.println("saved");
-//
-//
-//    }
-//}
+package mft.test;
+
+import mft.model.bl.CustomerBl;
+import mft.model.bl.EventBl;
+import mft.model.bl.PaymentBl;
+import mft.model.bl.TicketBl;
+import mft.model.entity.Customer;
+import mft.model.entity.Event;
+import mft.model.entity.Payment;
+import mft.model.entity.Ticket;
+import mft.model.entity.enums.EventCategory;
+import mft.model.entity.enums.PaymentType;
+
+import java.time.LocalDateTime;
+
+public class TicketTest {
+    public static void main(String[] args) throws Exception {
+        Customer customer=
+                Customer.builder()
+                        .name("khashayar")
+                        .family("mehdizadeh")
+                        .email("mzkhashayar@gmail.com")
+                        .phone_number("09120185685")
+
+                        .build();
+        CustomerBl.getCustomerBl().save(customer);
+        Event event=
+                Event.builder()
+                        .id(1)
+                        .name("Entry")
+                        .price(150.000)
+                        .capacity(500)
+                        .category(EventCategory.Jurassic)
+                        .description("friday")
+                        .build();
+        EventBl.getEventBl().save(event);
+        Payment payment=
+                Payment.builder()
+                        .id(1)
+                        .amount(150.000)
+                        .payment_type(PaymentType.Card)
+                        .date_time(LocalDateTime.now())
+                        .build();
+        PaymentBl.getPaymentBl().save(payment);
+        Ticket ticket=
+
+                Ticket.builder()
+                        .id(1)
+                        .info("Entry jurassic")
+                        .payment(payment)
+                        .customer(customer)
+                        .event(event)
+                        .build();
+        TicketBl.getTicketBl().save(ticket);
+
+
+
+        System.out.println("saved");
+
+
+    }
+}
